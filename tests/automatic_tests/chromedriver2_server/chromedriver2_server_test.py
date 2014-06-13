6669b25c8a41af207cfa95060bac7000d9299887
gitchs <tinyproxy@gmail.com>
2014-06-13 13:55:59 +0800

    Update chromedriver name and exit chromedriver with workaround API.

--- tests/automatic_tests/chromedriver2_server/chromedriver2_server_test.py
+++ tests/automatic_tests/chromedriver2_server/chromedriver2_server_test.py
@@ -3,24 +3,24 @@ import time
 import os
 from selenium import webdriver
 
-#path = os.getcwd();
-#path = os.path.join(path, 'tmp-nw', 'chromedriver2_server');
-path = '/home/owen-cloud/Desktop/kevin/node-webkit/tests/tmp-nw/chromedriver2_server';
+script_dir = os.path.dirname(__file__)
+path = os.path.join(script_dir,"..","..","tmp-nw","chromedriver2_server")
+path = os.path.abspath(path)
 
+driver = None
 try:
     driver = webdriver.Chrome(path);
     driver.get('http://www.google.com');
     assert driver.title == 'Google'
-
     time.sleep(5) # Let the user actually see something!
     search_box = driver.find_element_by_name('q')
     search_box.send_keys('ChromeDriver')
     search_box.submit()
     time.sleep(5) # Let the user actually see something!    
     assert driver.title[0:12] == 'ChromeDriver'
-    driver.quit()
 except:
-    driver.quit();
     traceback.print_exc()
 else:
     print 'pass'
+if driver is not None:
+    driver.close()
--- tests/automatic_tests/chromedriver2_server/mocha_test.js
+++ tests/automatic_tests/chromedriver2_server/mocha_test.js
@@ -1,7 +1,7 @@
-var path =  require('path');
+var path = require('path');
 var exec = require('child_process').exec;
 var assert = require('assert');
-var func = require('./' + global.tests_dir +'/start_app/script.js');
+var func = require('./' + global.tests_dir + '/start_app/script.js');
 var fs = require('fs-extra');
 var result = new Array();
 var child;
@@ -9,47 +9,47 @@ var isFileExist = true;
 describe('chromedriver2_server', function() {
     this.timeout(0);
     before(function(done) {
-	func.copyExecFiles(function () {
-	    //chromedriver2_server needs to be put under folder 'nw/tests/automatic_tests/chromedriver2_server'
-	    var src_file;
+        func.copyExecFiles(function() {
+            //chromedriver2_server needs to be put under folder 'nw/tests/automatic_tests/chromedriver'
+            var src_file;
             if (process.platform == 'win32')
-		src_file = path.join(global.tests_dir, 'chromedriver2_server', 'chromedriver2_server.exe');
-	    else
-		src_file = path.join(global.tests_dir, 'chromedriver2_server', 'chromedriver2_server');
-	    if (!fs.existsSync(src_file)) {
-		isFileExist = false;
-		done();
-	    }
-	    var dst_file = path.join('tmp-nw', 'chromedriver2_server');
-	    fs.copy(src_file, dst_file, function(err) {
-		if(err) throw err;
-		setTimeout(function () {
-		exec('python ' + path.join(global.tests_dir, 'chromedriver2_server','chromedriver2_server_test.py'),
-		     function (error, stdout, stderr) {
-			 result.push(error);
-			 result.push(stdout);
-			 result.push(stderr);
-			 done();
-		     });
-		}, 100);
-	    });
-	});
-    });
-    
-    after(function () {
-	fs.remove('tmp-nw', function (er) {
-            if (er) throw er;
-	});
+                src_file = path.join(global.tests_dir, 'chromedriver2_server', 'chromedriver.exe');
+            else
+                src_file = path.join(global.tests_dir, 'chromedriver2_server', 'chromedriver');
+            if (!fs.existsSync(src_file)) {
+                isFileExist = false;
+                done("chromedriver2_server needs to be put under folder 'nw/tests/automatic_tests/chromedriver'");
+            }
+            var dst_file = path.join('tmp-nw', 'chromedriver2_server');
+            fs.copy(src_file, dst_file, function(err) {
+                if (err) throw err;
+                setTimeout(function() {
+                    exec('python ' + path.join(global.tests_dir, 'chromedriver2_server', 'chromedriver2_server_test.py'),
+                        function(error, stdout, stderr) {
+                            result.push(error);
+                            result.push(stdout);
+                            result.push(stderr);
+                            done();
+                        });
+                }, 100);
+            });
+        });
     });
 
-    it('should work' ,function(done) {
-	if (!isFileExist)
-	    done("chromedrier2_server does not exist");
-	else {
-	    assert.equal(result[0], null);
-	    assert.equal(result[1], 'pass\n');
-	    assert.equal(result[2], '');
-	    done();
-	}
+    after(function() {
+        fs.remove('tmp-nw', function(er) {
+            if (er) throw er;
+        });
+    });
+
+    it('should work', function(done) {
+        if (!isFileExist)
+            done("chromedrier2_server does not exist");
+        else {
+            assert.equal(result[0], null);
+            assert.equal(result[1], 'pass\n');
+            assert.equal(result[2], '');
+            done();
+        }
     });
 });

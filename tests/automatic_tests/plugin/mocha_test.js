2c1da474e9a1206bccb20d3231340a20ce2f23f5
gitchs <tinyproxy@gmail.com>
2014-08-11 09:33:21 +0800

    Fix flash test case

--- tests/automatic_tests/plugin/mocha_test.js
+++ tests/automatic_tests/plugin/mocha_test.js
@@ -3,26 +3,31 @@ var app_test = require('./nw_test_app');
 
 describe('Plugin', function() {
   describe('flash', function(){
+    this.timeout(0);
     it('should not crash', function(done) {
       var result = false;
+      var crontab = undefined;
 
       var child = app_test.createChildProcess({
         execPath: process.execPath,
         appPath: path.join(global.tests_dir, 'plugin', 'flash'),
         end: function(data, app) {        
+          if (crontab){
+            clearTimeout(crontab);
+          }
           result = true;
           app.kill();
           done();
         }
       });
 
-      setTimeout(function(){
+      crontab = setTimeout(function(){
         if (!result) {
           done('nw crash.');
           child.close();
           //child.removeConnection();
         }
-      }, 2000);
+      }, 4000);
 
 
     })
